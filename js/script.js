532f9b64f58a30dd293427172d01bbac43398247
thealamindev <ll.alamin.ll@gmail.com>
2025-03-10 08:57:59 +0600

    Add scnd logic

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let mainScreen = document.querySelector(".mainScreen");
 let nameInput = document.querySelector(".nameInput");
 let playBtn = document.querySelector(".playBtn");
 let nameError = document.querySelector(".nameError");
-let heading = document.querySelector(".heading")
+let heading = document.querySelector(".heading");
 // =============
 // Player One Screen
 
@@ -14,9 +14,8 @@ let playerOneError = document.querySelector(".playerOneError");
 playBtn.addEventListener("click", function () {
   if (nameInput.value == "") {
     nameError.style.display = "block";
-  }
-   else {
-    heading.innerHTML= "Player One"
+  } else {
+    heading.innerHTML = "Player One";
     playerOneScreen.style.display = "block";
     mainScreen.style.display = "none";
   }
@@ -24,14 +23,20 @@ playBtn.addEventListener("click", function () {
 
 // ====================
 playerPlayBtn.addEventListener("click", function () {
-  if (playerOneInput.value == "") {
-    playerOneError.style.display = "block";
+  // if (playerOneInput.value == "") {
+  //   playerOneError.style.display = "block";
+  // }
+  if (playerOneInput.value < 1 || playerOneInput.value > 10) {
+    console.log("Error");
   }
-//    else {
-//     heading.innerHTML= "Player One"
-//     playerOneScreen.style.display = "block";
-//     mainScreen.style.display = "none";
-//   }
+   else {
+    if (playerOneInput.value - 15) {
+      console.log("Ami Number and 3rd screen a jao");
+    }
+     else {
+      console.log("Ami Number Na");
+    }
+  }
 });
 
 // ======================
